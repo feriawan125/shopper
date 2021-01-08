2a046913f6dd8e3df86e4d36e5599c4e27393b6d
feriawan125 <ferimario6@gmail.com>
2021-01-09 04:12:12 +0700

    Auto By Feriawan

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f4dd41a4b0f40b251276ffb7b18c9cf6a972326a',
+    'name' => 'almasaeed2010/adminlte',
+  ),
+  'versions' => 
+  array (
+    'almasaeed2010/adminlte' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f4dd41a4b0f40b251276ffb7b18c9cf6a972326a',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.0',
+      'version' => '5.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'feb0e820b8436873675fd3aca04f3728eb2185cb',
+    ),
+  ),
+);
